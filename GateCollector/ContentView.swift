341c1e64c2a1ae437641abaabd34eb8cc185f1a1
James D <40120130+JamesXDO@users.noreply.github.com>
2024-10-21 19:00:35 +0700

    Initial Commit

---
+++ GateCollector/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GateCollector
+//
+//  Created by Nonprawich I. on 21/10/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GateCollector/GateCollectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  GateCollectorApp.swift
+//  GateCollector
+//
+//  Created by Nonprawich I. on 21/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct GateCollectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
